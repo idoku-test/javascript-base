a6acb870fa7f3db926d48aebaec960b5ff8c4484
doku <dragon.ice@foxmail.com>
2019-10-21 17:51:49 +0800

    test

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log("我是控制台呀看到我了吗"); //控制台输出
+console.log('-----------')
+function f(){}
+console.log(typeof f)
+console.log(typeof {})
+console.log(typeof [])
+console.log('-----------')
+console.log(typeof null)
+console.log(typeof undefined)
+console.log('-----------')
+if(!undefined)
+    console.log('undefined is false')
+
+if(!null)
+    console.log('null is false')
+console.log('-----------')
+console.log(undefined == null)    
+console.log(undefined === null)    
