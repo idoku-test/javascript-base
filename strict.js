17c8104c6d44a9c505ec44900da3d73072b4f686
idoku <dragon.ice@foxmail.com>
2019-11-01 00:18:01 +0800

    add

--- strict.js
+++ strict.js
@@ -23,8 +23,61 @@
 // }
 // f()
 
-function f(){
+// function f(){
+//     'use strict'
+//     this.a = 1;
+// };
+// f()
+
+// function fun()
+// {
+//     return this;
+// }
+// //console.log(fun())
+// console.log(fun.call(2))
+// console.log(fun.call(true))
+// console.log(fun.call(null))
+// console.log(fun.call(undefined))
+
+// function fun()
+// {
+//     'use strict'
+//     return this;
+// }
+// //console.log(fun())
+// console.log(fun.call(2))
+// console.log(fun.call(true))
+// console.log(fun.call(null))
+// console.log(fun.call(undefined))
+
+// function f1(){
+//     'use strict'
+//     f1.caller;
+// }
+
+// f1()
+
+// function f1(){
+//     'use strict'
+//     return arguments.callee;
+// }
+// f1()
+
+// 'use strict'
+// var x;
+// delete x;
+
+// (function () {
+//     //'use strict';
+//     var x = 2;
+//     console.log(eval('var x = 5; x')) // 5
+//     console.log(x) // 2
+//   })()
+
+function f(a)
+{
     'use strict'
-    this.a = 1;
-};
-f()
+    a = 2;
+    return [a,arguments[0]];
+}
+console.log(f(1))
